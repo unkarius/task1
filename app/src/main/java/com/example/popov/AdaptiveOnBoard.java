6c9ae25ad65a55e2dce2273a108e7e472d6e0172
rmp <polinalinkova2608@mail.ru>
2023-10-02 15:54:04 +0300

    bdsfgtrf

---
+++ app/src/main/java/com/example/popov/AdaptiveOnBoard.java
@@ -0,0 +1,14 @@
+package com.example.popov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdaptiveOnBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adaptive_on_board);
+    }
+}
